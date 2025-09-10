09cdda3608edacd060b22284ae04a6bf33061edd
ignpewnie <ignpewnie@gmail.com>
2025-09-10 14:11:35 +0200

    Create gameload.lua

---
+++ gameload.lua
@@ -0,0 +1,13 @@
+if game.PlaceId == 118915549367482 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ignpewnie/archhub/refs/heads/main/DontWakeUpBrainrot.lua"))()
+elseif game.PlaceId == 111386538027749 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ignpewnie/archhub/refs/heads/main/StealCookies.lua"))()
+elseif game.PlaceId == 113604074601559 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ignpewnie/archhub/refs/heads/main/BuildaBeehive.lua"))()
+elseif game.PlaceId == 128886701132186 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ignpewnie/archhub/refs/heads/main/CaseRollingRng.lua"))()
+elseif game.PlaceId == 87700573492940 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ignpewnie/archhub/refs/heads/main/untitleddrillgame.lua"))()
+else
+    print("Wrong game ID!")
+end
